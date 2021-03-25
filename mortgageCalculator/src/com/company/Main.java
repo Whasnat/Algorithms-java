4a6fe37600aff7d34065c00c0402c006315869e2
whasnat <w.hasnat94@gmail.com>
2021-03-25 22:18:00 +0600

    Mortgage calculator..

--- mortgageCalculator/src/com/company/Main.java
+++ mortgageCalculator/src/com/company/Main.java
@@ -6,23 +6,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter Principal Amount: "); int amount = input.nextInt();
-        System.out.println("Enter Interest rate: "); double rate = input.nextDouble();
-        System.out.println("Enter Period (Years): "); int period = input.nextInt();
+        System.out.println("Enter Principal Amount: ");
+        int amount = input.nextInt();
+        System.out.println("Enter Interest rate: ");
+        double rate = input.nextDouble();
+        System.out.println("Enter Period (Years): ");
+        int period = input.nextInt();
 
-        double mortgage = calculateMortgage(amount, rate ,period);
+        double mortgage = calculateMortgage(amount, rate, period);
         String totalPayable = NumberFormat.getCurrencyInstance().format(mortgage);
 
-        System.out.println("Mortgage: " +totalPayable);
+        System.out.println("Mortgage: " + totalPayable);
+
+        System.out.println("Math.Pow: "+Math.pow(5,2));
     }
 
-    private static double calculateMortgage(int amount, double interestRate, int period){
-        int p = amount;
-        double r = (interestRate/100);
-        int n = (period*12);
-        double mortgage = (p*((r*Math.pow((1+r),n))/(Math.pow((1+r),n)-1)));
-        return mortgage;
+    private static double calculateMortgage(int amount, double interestRate, int period) {
+        double r = (interestRate / 100); // interest rate
+        int n = (period * 12); // number of payments
+        return (amount * ((r * Math.pow((1 + r), n)) / (Math.pow((1 + r), n) - 1)));
     }
+
+//    private static void function(){}
 }
