444998920fead99a2b7ebb723a4480ee601f4222
whasnat <w.hasnat94@gmail.com>
2021-03-27 00:41:34 +0600

    Mortgage Calculator 2.0

--- mortgageCalculator/src/com/company/Main.java
+++ mortgageCalculator/src/com/company/Main.java
@@ -9,32 +9,41 @@ public class Main {
 
     public static void main(String[] args) {
 
-        takeInput();
-
-        double mortgage();
-        String totalPayable = NumberFormat.getCurrencyInstance().format(mortgage);
-
-        System.out.println("Mortgage: " + totalPayable);
-
-        System.out.println("Math.Pow: "+Math.pow(5,2));
+        double totalPayable = calculateMortgage();
+          System.out.println("Monthly Payable: " + totalPayable);
+        System.out.println(2506.689253401793*12*20);
     }
 
-    private static double calculateMortgage(int amount, double interestRate, int period) {
-        double r = (interestRate / 100); // interest rate
-        int n = (period * 12); // number of payments
-        return (amount * ((r * Math.pow((1 + r), n)) / (Math.pow((1 + r), n) - 1)));
-    }
-
-    private static void takeInput(){
+    private static double calculateMortgage(){
         Scanner input = new Scanner(System.in);
+        int amount, period;
+        float rate;
         while(true){
             System.out.println("Principal ($1K - $1M): ");
-            int amount = input.nextInt();
-
-            System.out.println("Enter Interest rate: ");
-            double rate = input.nextDouble();
-            System.out.println("Enter Period (Years): ");
-            int period = input.nextInt();
+            amount = input.nextInt();
+            if(1000<=amount && amount<=1_000_000)
+                break;
+            System.out.println("Value must be between 1,000 and 1,000,000");
         }
+        while(true){
+            System.out.println("Annual Interest: ");
+            rate = input.nextFloat();
+            if(rate>0 && rate<=30) {
+                rate /= 100;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+        while(true){
+            System.out.println("Period (5 years - 50 years): ");
+            period = input.nextInt();
+            if(period>=5 && period<=50) {
+                period *= 12;
+                break;
+            }
+            System.out.println("Enter a value between 5 and 50");
+        }
+
+        return (amount * ((rate * Math.pow((1 + rate), period)) / (Math.pow((1 + rate), period) - 1)));
     }
 }
